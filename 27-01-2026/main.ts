5cb4fb04f142c86464b3636d234282257d7772e6
Trang130201 <nguyenthuytrang.vtvn@gmail.com>
2026-02-07 03:44:59 +0700

    add missing interface and use throw error instead of console.error

--- 27-01-2026/main.ts
+++ 27-01-2026/main.ts
@@ -1,3 +1,10 @@
+interface EmployeeI {
+    getId:() => number
+    getName:() => string
+    setName:(name: string) => void
+    setSalary:(salary: number) => void
+    calculateSalary:() => number
+}
 // Abstract Class Employee
 abstract class Employee {
     id: number
@@ -8,7 +15,7 @@ abstract class Employee {
     constructor(id: number, name: string, salary: number) {
         this.id = id
         this.name = name
-        this.salary = salary
+        this.setSalary(salary)
     }
 
     // Get employee Id
@@ -23,7 +30,7 @@ abstract class Employee {
 
     // Set employee name
     public setName(name: string) {
-        return this.name = name
+        this.name = name
     }
 
     // Get employee salary
@@ -34,8 +41,7 @@ abstract class Employee {
     // Set employee salary with validation
     public setSalary(salary: number){
         if (salary <= 0 ){
-           console.error ( "Salary must be greater than 0")
-           return
+           throw new Error ( "Salary must be greater than 0")
         }
         this.salary = salary
     }
@@ -45,8 +51,8 @@ abstract class Employee {
 }
 
 
-// Developer class extends Employee
-class Developer extends Employee {
+
+class Developer extends Employee implements EmployeeI {
 
     // Overtime hours property
     overtimeHours: number
@@ -65,8 +71,8 @@ class Developer extends Employee {
 }
 
 
-// Manager class extends Employee
-class Manager extends Employee {
+
+class Manager extends Employee implements EmployeeI {
 
     // Team size property
     teamSize: number
@@ -85,33 +91,45 @@ class Manager extends Employee {
 }
 
 // Create instance developer 
-const developer = new Developer(1, "Linh", 100, 10)
-console.log(developer) //Developer { id: 1, name: 'Linh', salary: 100, overtimeHours: 10 }
+const developer: EmployeeI = new Developer(1, "Linh", -200, 10)
+console.log(developer) 
+//Developer { id: 1, name: 'Linh', salary: 100, overtimeHours: 10 }
 
 // Print calculate salary of developer
-console.log(developer.calculateSalary()); //500100
+console.log(developer.calculateSalary()); 
+//500100
 
 // Update developer name and salary
 developer.setName("Mai")
 developer.setSalary(200)
 
-console.log(developer); // Developer { id: 1, name: 'Mai', salary: 200, overtimeHours: 10 }
+console.log(developer); 
+// Developer { id: 1, name: 'Mai', salary: 200, overtimeHours: 10 }
 
-// Print developer Id
-console.log(developer.getId()) // 1
+
+console.log(developer.getId()) 
+// 1
 
 
 // Create instance manager
-const manager = new Manager(2, "An", 200, 5)
-console.log(manager) //Manager { id: 2, name: 'An', salary: 200, teamSize: 5 }
+const manager: EmployeeI = new Manager(2, "An", 200, 5)
+console.log(manager) 
+//Manager { id: 2, name: 'An', salary: 200, teamSize: 5 }
 
 // Print calculate salary of manager
-console.log(manager.calculateSalary()); //1000200
+console.log(manager.calculateSalary()); 
+//1000200
 
 // Update manager name and salary
 manager.setName("Binh")
 manager.setSalary(300)
-console.log(manager); // Manager { id: 2, name: 'Binh', salary: 300, teamSize: 5 }
+console.log(manager); 
+// Manager { id: 2, name: 'Binh', salary: 300, teamSize: 5 }
 
 // Print manager name
-console.log(manager.getName()) //Binh
+console.log(manager.getName()) 
+//Binh
+
+// Print error case when salary less than or equal to 0
+manager.setSalary(-100)
+//throw new Error ( "Salary must be greater than 0")
